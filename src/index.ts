5f18bc4ad63655e02311f966f0a637ba4b67fe88
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2023-10-02 12:42:38 -0600

    fix: remove commander?

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,51 @@
 #! /usr/bin/env node
+import { printDefinitions } from './printers/printDefinitions'
+import { parseDefinitions } from './parsers/parseDefinitions'
+import { userInterface } from './util/interface'
+import {
+    setDefinitions,
+    setUserSettings,
+    getDefinitions,
+    getUserSettings,
+    setParameters,
+    setRoutes,
+    getParameters,
+    getRoutes,
+    getResponses,
+    setResponses, getTags
+} from './util/globals'
+import { parseRoutes } from './parsers/parseRoutes'
+import { parseParameters } from './parsers/parseParameters'
+import { readJsonFromFile, setupGenDir } from './util/util'
+import { SwaggerDoc } from './util/swagger'
+import { printParameters } from './printers/printParameters'
+import { printRoutes } from './printers/printRoutes'
+import { printResponses } from './printers/printResponses'
+import { parseResponses } from './parsers/parseResponses'
+import { printTags } from './classes/tags'
+import { topologicalSort } from './util/topologicalSort'
+import { getFileSettings } from './util/getFileSettings'
 
-import { Command } from 'commander'
-import { run } from './run'
-const program = new Command()
+export async function run (): Promise<void> {
+    if (process.argv.length > 3 && process.argv[3] === '-f') {
+        setUserSettings(await getFileSettings())
+    } else {
+        setUserSettings(await userInterface())
+    }
 
-program
-  .name('fastgen')
+    const openApiJSON: SwaggerDoc = await readJsonFromFile(getUserSettings().filePath) as SwaggerDoc
+    setDefinitions(parseDefinitions(openApiJSON))
+    setParameters(parseParameters(openApiJSON))
+    setResponses(parseResponses(openApiJSON))
+    setRoutes(parseRoutes(openApiJSON))
+    await setupGenDir()
+    // we need to find a valid ordering of definitions in the same file, so we do a topological sort on the dependency graph
+    setDefinitions(topologicalSort(getDefinitions()))
+    await printDefinitions(getDefinitions())
+    await printParameters(getParameters())
+    await printResponses(getResponses())
+    await printTags(getTags())
+    await printRoutes(getRoutes())
+}
 
-program.command('run')
-  .option('-f, use a file for settings instead of prompting the user')
-  .action(run)
-
-program.parse()
+void run()
