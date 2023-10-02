da9012d175d8cedd2ed4769217888e6a28741968
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2023-10-02 12:43:09 -0600

    fix: remove commander?

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import { printDefinitions } from './printers/printDefinitions'
 import { parseDefinitions } from './parsers/parseDefinitions'
 import { userInterface } from './util/interface'
 import {
-    setDefinitions,
-    setUserSettings,
-    getDefinitions,
-    getUserSettings,
-    setParameters,
-    setRoutes,
-    getParameters,
-    getRoutes,
-    getResponses,
-    setResponses, getTags
+  setDefinitions,
+  setUserSettings,
+  getDefinitions,
+  getUserSettings,
+  setParameters,
+  setRoutes,
+  getParameters,
+  getRoutes,
+  getResponses,
+  setResponses, getTags
 } from './util/globals'
 import { parseRoutes } from './parsers/parseRoutes'
 import { parseParameters } from './parsers/parseParameters'
@@ -27,25 +27,25 @@ import { topologicalSort } from './util/topologicalSort'
 import { getFileSettings } from './util/getFileSettings'
 
 export async function run (): Promise<void> {
-    if (process.argv.length > 3 && process.argv[3] === '-f') {
-        setUserSettings(await getFileSettings())
-    } else {
-        setUserSettings(await userInterface())
-    }
+  if (process.argv.length > 3 && process.argv[3] === '-f') {
+    setUserSettings(await getFileSettings())
+  } else {
+    setUserSettings(await userInterface())
+  }
 
-    const openApiJSON: SwaggerDoc = await readJsonFromFile(getUserSettings().filePath) as SwaggerDoc
-    setDefinitions(parseDefinitions(openApiJSON))
-    setParameters(parseParameters(openApiJSON))
-    setResponses(parseResponses(openApiJSON))
-    setRoutes(parseRoutes(openApiJSON))
-    await setupGenDir()
-    // we need to find a valid ordering of definitions in the same file, so we do a topological sort on the dependency graph
-    setDefinitions(topologicalSort(getDefinitions()))
-    await printDefinitions(getDefinitions())
-    await printParameters(getParameters())
-    await printResponses(getResponses())
-    await printTags(getTags())
-    await printRoutes(getRoutes())
+  const openApiJSON: SwaggerDoc = await readJsonFromFile(getUserSettings().filePath) as SwaggerDoc
+  setDefinitions(parseDefinitions(openApiJSON))
+  setParameters(parseParameters(openApiJSON))
+  setResponses(parseResponses(openApiJSON))
+  setRoutes(parseRoutes(openApiJSON))
+  await setupGenDir()
+  // we need to find a valid ordering of definitions in the same file, so we do a topological sort on the dependency graph
+  setDefinitions(topologicalSort(getDefinitions()))
+  await printDefinitions(getDefinitions())
+  await printParameters(getParameters())
+  await printResponses(getResponses())
+  await printTags(getTags())
+  await printRoutes(getRoutes())
 }
 
 void run()
