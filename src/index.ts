e70d48973f667f58675fbbe91091e06cf7eb7ad3
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2023-10-02 11:22:55 -0600

    fix: add commander

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,12 @@
-import { printDefinitions } from './printers/printDefinitions'
-import { parseDefinitions } from './parsers/parseDefinitions'
-import { userInterface } from './util/interface'
-import {
-  setDefinitions,
-  setUserSettings,
-  getDefinitions,
-  getUserSettings,
-  setParameters,
-  setRoutes,
-  getParameters,
-  getRoutes,
-  getResponses,
-  setResponses, getTags
-} from './util/globals'
-import { parseRoutes } from './parsers/parseRoutes'
-import { parseParameters } from './parsers/parseParameters'
-import { readJsonFromFile, setupGenDir } from './util/util'
-import { SwaggerDoc } from './util/swagger'
-import { printParameters } from './printers/printParameters'
-import { printRoutes } from './printers/printRoutes'
-import { printResponses } from './printers/printResponses'
-import { parseResponses } from './parsers/parseResponses'
-import { printTags } from './classes/tags'
-import { topologicalSort } from './util/topologicalSort'
-import { getFileSettings } from './util/getFileSettings'
+import { Command } from 'commander'
+const program = new Command()
+import { run } from './run'
 
-async function run (): Promise<void> {
-  if (process.argv.length > 2 && process.argv[2] === '--f') {
-    setUserSettings(await getFileSettings())
-  } else {
-    setUserSettings(await userInterface())
-  }
+program
+    .name('fastgen')
 
-  const openApiJSON: SwaggerDoc = await readJsonFromFile(getUserSettings().filePath) as SwaggerDoc
-  setDefinitions(parseDefinitions(openApiJSON))
-  setParameters(parseParameters(openApiJSON))
-  setResponses(parseResponses(openApiJSON))
-  setRoutes(parseRoutes(openApiJSON))
-  await setupGenDir()
-  // we need to find a valid ordering of definitions in the same file, so we do a topological sort on the dependency graph
-  setDefinitions(topologicalSort(getDefinitions()))
-  await printDefinitions(getDefinitions())
-  await printParameters(getParameters())
-  await printResponses(getResponses())
-  await printTags(getTags())
-  await printRoutes(getRoutes())
-}
+program.command('')
+    .option('-f, use file')
+    .action(run)
 
-void run()
+program.parse()
---
+++ src/run.ts
@@ -0,0 +1,48 @@
+import { printDefinitions } from './printers/printDefinitions'
+import { parseDefinitions } from './parsers/parseDefinitions'
+import { userInterface } from './util/interface'
+import {
+  setDefinitions,
+  setUserSettings,
+  getDefinitions,
+  getUserSettings,
+  setParameters,
+  setRoutes,
+  getParameters,
+  getRoutes,
+  getResponses,
+  setResponses, getTags
+} from './util/globals'
+import { parseRoutes } from './parsers/parseRoutes'
+import { parseParameters } from './parsers/parseParameters'
+import { readJsonFromFile, setupGenDir } from './util/util'
+import { SwaggerDoc } from './util/swagger'
+import { printParameters } from './printers/printParameters'
+import { printRoutes } from './printers/printRoutes'
+import { printResponses } from './printers/printResponses'
+import { parseResponses } from './parsers/parseResponses'
+import { printTags } from './classes/tags'
+import { topologicalSort } from './util/topologicalSort'
+import { getFileSettings } from './util/getFileSettings'
+
+export async function run (useFile: string): Promise<void> {
+  if (useFile != null && useFile === '-f') {
+    setUserSettings(await getFileSettings())
+  } else {
+    setUserSettings(await userInterface())
+  }
+
+  const openApiJSON: SwaggerDoc = await readJsonFromFile(getUserSettings().filePath) as SwaggerDoc
+  setDefinitions(parseDefinitions(openApiJSON))
+  setParameters(parseParameters(openApiJSON))
+  setResponses(parseResponses(openApiJSON))
+  setRoutes(parseRoutes(openApiJSON))
+  await setupGenDir()
+  // we need to find a valid ordering of definitions in the same file, so we do a topological sort on the dependency graph
+  setDefinitions(topologicalSort(getDefinitions()))
+  await printDefinitions(getDefinitions())
+  await printParameters(getParameters())
+  await printResponses(getResponses())
+  await printTags(getTags())
+  await printRoutes(getRoutes())
+}
